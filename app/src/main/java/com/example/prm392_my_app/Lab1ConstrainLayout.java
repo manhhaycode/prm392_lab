da2ce7b43a1a51529b10def0d8d7aa52e68fe333
manhhaycode <manhnvse173470@fpt.edu.vn>
2025-02-08 07:03:56 +0700

    up

---
+++ app/src/main/java/com/example/prm392_my_app/Lab1ConstrainLayout.java
@@ -0,0 +1,19 @@
+package com.example.prm392_my_app;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Lab1ConstrainLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_lab1_constrain_layout);
+    }
+}
---
+++ app/src/main/java/com/example/prm392_my_app/Lab1LinearLayoutB.java
@@ -0,0 +1,17 @@
+package com.example.prm392_my_app;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Lab1LinearLayoutB extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_lab1_linear_layout_b);
+
+    }
+}
